d0714489f4d7e5da42bb4dde8238362e3906725c
Dan Teesdale <danteesdale@gmail.com>
2026-03-15 16:32:41 -0400

    add deploy wf

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === 'development';
+
 const config = {
 	kit: {
 		adapter: adapter({
@@ -9,7 +11,10 @@ const config = {
 			fallback: 'index.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/durable-showcase'
+		}
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
